70c59512617e2081c54ab13ae31829c33590d68a
apichat thongngoen <37826468+apichatthongngoen@users.noreply.github.com>
2020-04-29 12:34:56 +0700

    ทำ page 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { NotfoundComponent } from './error/notfound/notfound.component'; //404
+
 const routes: Routes = [
-  { path: '', redirectTo: 'admin', pathMatch: 'full' }
+  { path: '', redirectTo: 'admin', pathMatch: 'full' },
+  { path: '404', component: NotfoundComponent }
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagesModule } from './pages/pages.module';
+
 // routing
 import { LayoutComponent } from './layout/layout/layout.component';
 
