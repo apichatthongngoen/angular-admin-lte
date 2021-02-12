e155c82fd7dd3fa95ded112c86a35cc800812ee4
apichat thongngoen <37826468+apichatthongngoen@users.noreply.github.com>
2021-02-12 14:49:11 +0700

    ประกาศชื่อ v1

--- src/app/display/display1/display1.component.ts
+++ src/app/display/display1/display1.component.ts
@@ -1,26 +1,62 @@
 import { Component, OnInit } from '@angular/core';
 
+import { GetDataServiceService } from '../../shared/get-data-service.service';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { InterfaceScreenCalledQueue } from '../../shared/interface/dataapi';
+import { timer } from 'rxjs';
+import { Observable } from 'rxjs/internal/Observable';
+
 @Component({
   selector: 'app-display1',
   templateUrl: './display1.component.html',
-  styleUrls: ['./display1.component.scss']
+  styleUrls: ['./display1.component.scss'],
 })
 export class Display1Component implements OnInit {
-
-  constructor() { }
+  public results: InterfaceScreenCalledQueue[]; //
+  constructor(
+    public getDataServiceService: GetDataServiceService,
+    private http: HttpClient
+  ) {}
 
   ngOnInit(): void {
+    const source = timer(100, 1000);
+    // console.log(source);.
+    const subscribe = source.subscribe((val) => {
+      this.loadData(val);
+    });
+
+    this.announceQueuing(source);
+
+    // this.results.su
+    // console.log(subscribe);
   }
-  qndisplay1 = [
-    { "id": 1, "qn": "1-001", "dot": "5", "bgcolor": "primary-rx" },
-    { "id": 1, "qn": "1-002", "dot": "5", "bgcolor": "primary-rx" },
-    { "id": 1, "qn": "1-003", "dot": "5", "bgcolor": "primary-rx" },
-    { "id": 1, "qn": "1-004", "dot": "5", "bgcolor": "primary-rx" },
-    { "id": 1, "qn": "1-005", "dot": "5", "bgcolor": "primary-rx" },
 
+  loadData(val: any) {
+    // this.results = data1;
+    this.getDataServiceService
+      .getDisPay(
+        `screencalledqueu?uiDisplay=PharmacyScreenCalledQueue&idServicePoint=1`
+      )
+      .subscribe((data: any) => {
+        this.results = data;
+        console.log(val);
+        console.log('*********----------*********');
+        console.log(data);
+        // console.log(this.announceQueuing());
+      });
 
+    // await setTimeout(async () => {}, 50000);
+    return val;
+  }
 
-  ];
-
+  announceQueuing(val: Observable<number>) {
+    val.subscribe((val) => {
+      console.log('val');
+    });
+    // console.log(val);
+    // setTimeout(function () {
+    //   console.log('setTimeout');
+    // }, 10000);
+    return 'setTimeout2';
+  }
 }
-
