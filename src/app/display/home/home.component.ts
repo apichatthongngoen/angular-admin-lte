4bb54c5e1964e078e9184f3da9d7ea484333e495
apichat thongngoen <37826468+apichatthongngoen@users.noreply.github.com>
2021-02-12 15:29:49 +0700

    home v1

--- src/app/display/home/home.component.ts
+++ src/app/display/home/home.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 
+import { GetDataServiceService } from '../../shared/get-data-service.service';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { timer } from 'rxjs';
+
+import { QueueItem } from '../../shared/interface/dataapi';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  public results: QueueItem[]; //
 
-  constructor() { }
+  constructor(
+    public getDataServiceService: GetDataServiceService,
+    private http: HttpClient
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  pushCreateQueue(event, inputHNSearch: string) {
+    // console.log(event);
+    // console.log(inputHNSearch);
+    let data = {
+      id: '1',
+      vn: inputHNSearch,
+    };
+    console.log(data);
+    this.getDataServiceService
+      .postAPI(`createqueue`, data)
+      .subscribe((data) => {
+        // this.results = data;
+        console.log(data);
+        alert(data.nameQueue || 'ERROR');
+        // console.log(val)
+      });
+    return;
   }
-
 }
