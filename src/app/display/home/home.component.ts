6d148ea0e7796eb9c975fe0e249774ffd1e2faf9
apichat thongngoen <37826468+apichatthongngoen@users.noreply.github.com>
2021-02-15 12:24:17 +0700

    inputHNSearchkeypress map

--- src/app/display/home/home.component.ts
+++ src/app/display/home/home.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 import { GetDataServiceService } from '../../shared/get-data-service.service';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { timer } from 'rxjs';
 
-import { QueueItem } from '../../shared/interface/dataapi';
+import { QueueItem, QueueGroupPrint } from '../../shared/interface/dataapi';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -13,30 +14,59 @@ import { QueueItem } from '../../shared/interface/dataapi';
 })
 export class HomeComponent implements OnInit {
   public results: QueueItem[]; //
-
+  public queueGroupPrint: QueueGroupPrint;
+  inputHNSearchVal: string = '';
+  @ViewChild('inputHNSearch') inputHNSearch: ElementRef;
   constructor(
     public getDataServiceService: GetDataServiceService,
     private http: HttpClient
   ) {}
 
   ngOnInit(): void {}
+  private charArray: [number, string][] = [
+    [3592, '0'],
+    [3653, '1'],
+    [47, '2'],
+    [45, '3'],
+    [3616, '4'],
+    [3606, '5'],
+    [3640, '6'],
+    [3638, '7'],
+    [3588, '8'],
+    [3605, '9'],
+  ];
+  private charMap: Map<number, string> = new Map(this.charArray);
 
   pushCreateQueue(event, inputHNSearch: string) {
     // console.log(event);
     // console.log(inputHNSearch);
     let data = {
       id: '1',
-      vn: inputHNSearch,
+      hn: inputHNSearch,
     };
-    console.log(data);
     this.getDataServiceService
       .postAPI(`createqueue`, data)
-      .subscribe((data) => {
+      .subscribe((data: any) => {
         // this.results = data;
-        console.log(data);
-        alert(data.nameQueue || 'ERROR');
+        this.queueGroupPrint = data;
+        // alert(data.nameQueue || 'ERROR');
+
         // console.log(val)
       });
     return;
+  }
+  inputHNSearchkeypress(event) {
+    this.keyMap(event);
+  }
+
+  keyMap(event) {
+    if (this.charMap.get(event.charCode) != null) {
+      event.preventDefault();
+      // this.inputHNSearchVal.val(
+      //   this.inputHNSearchVal.val() + thsi.charMap.get(event.charCode)
+      // );
+      this.inputHNSearchVal =
+        this.inputHNSearchVal + this.charMap.get(event.charCode);
+    }
   }
 }
