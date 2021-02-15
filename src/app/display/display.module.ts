650624b1aedd6ff8c6172effd77f76f16950b00f
apichat thongngoen <37826468+apichatthongngoen@users.noreply.github.com>
2021-02-15 12:40:10 +0700

    add FormsModule

--- src/app/display/display.module.ts
+++ src/app/display/display.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { DisplayRoutingModule } from './display-routing.module';
 import { HomeComponent } from './home/home.component';
 
-
 @NgModule({
   declarations: [HomeComponent],
-  imports: [
-    CommonModule,
-    DisplayRoutingModule
-  ]
+  imports: [CommonModule, DisplayRoutingModule, FormsModule],
 })
-export class DisplayModule { }
+export class DisplayModule {}
