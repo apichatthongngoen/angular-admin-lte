13cc8ab4d34faa8400032580e66f4e017f74e6c4
apichat thongngoen <37826468+apichatthongngoen@users.noreply.github.com>
2021-03-08 21:16:04 +0700

    แก้ ช่องบริการ

--- src/app/pages/pharmacy/queue-pharmacy/queue-pharmacy-main/queue-pharmacy-main.component.ts
+++ src/app/pages/pharmacy/queue-pharmacy/queue-pharmacy-main/queue-pharmacy-main.component.ts
@@ -1,38 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import { MatSelectModule } from '@angular/material/select';
+import { MatSelectModule } from "@angular/material/select";
 
-import { GetDataServiceService } from '../../../../shared/get-data-service.service';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { timer } from 'rxjs';
+import { GetDataServiceService } from "../../../../shared/get-data-service.service";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { timer } from "rxjs";
 
-import { ServiceChannel } from '../../../../shared/interface/dataapi';
+import { ServiceChannel } from "../../../../shared/interface/dataapi";
 
 @Component({
-  selector: 'app-queue-pharmacy-main',
-  templateUrl: './queue-pharmacy-main.component.html',
-  styleUrls: ['./queue-pharmacy-main.component.scss'],
+  selector: "app-queue-pharmacy-main",
+  templateUrl: "./queue-pharmacy-main.component.html",
+  styleUrls: ["./queue-pharmacy-main.component.scss"],
 })
 export class QueuePharmacyMainComponent implements OnInit {
-  public results: ServiceChannel;
+  public resultsServiceChannels: ServiceChannel;
   public selectedValueServiceChannel;
-  constructor(
-    public GetDataServiceService: GetDataServiceService,
-    private http: HttpClient
-  ) {}
+  constructor(public GetDataServiceService: GetDataServiceService, private http: HttpClient) {}
 
   ngOnInit(): void {
     this.loadData();
   }
 
-  public name_container_header = 'คิวการเรียกรับยา';
-  public tabsGroup2 = '2';
-  public tabsGroup3 = '3';
-  public tabsGroup4 = '4';
-  public tabsGroup5 = '5';
-  public tabsGroup1 = '1';
-  public tabsGroup6 = '6';
-  public tabsGroup7 = '6';
+  public name_container_header = "คิวการเรียกรับยา";
+  public tabsGroup2 = "2";
+  public tabsGroup3 = "3";
+  public tabsGroup4 = "4";
+  public tabsGroup5 = "5";
+  public tabsGroup1 = "1";
+  public tabsGroup6 = "6";
+  public tabsGroup7 = "6";
 
   loadData() {
     // this.results = data1;
@@ -40,8 +37,9 @@ export class QueuePharmacyMainComponent implements OnInit {
     return this.GetDataServiceService.getDisPay(
       `servicechannel?test={"idServicePoint":1,"idServicePointSub":3}`
     ).subscribe((data: any) => {
-      this.results = data;
-      console.log(this.results);
+      this.resultsServiceChannels = data;
+      console.log(" room :");
+      console.log(this.resultsServiceChannels);
     });
   }
 }
