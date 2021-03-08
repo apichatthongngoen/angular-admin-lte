7a6f917b2d3397748c4e7b93235541cc770bd05f
apichat thongngoen <37826468+apichatthongngoen@users.noreply.github.com>
2021-03-08 20:57:13 +0700

    การเรียกเสียง

--- src/app/display/display1/display1.component.ts
+++ src/app/display/display1/display1.component.ts
@@ -1,47 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import { GetDataServiceService } from '../../shared/get-data-service.service';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { InterfaceScreenCalledQueue } from '../../shared/interface/dataapi';
-import { timer } from 'rxjs';
-import { Observable } from 'rxjs/internal/Observable';
+import { GetDataServiceService } from "../../shared/get-data-service.service";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { InterfaceScreenCalledQueue } from "../../shared/interface/dataapi";
+import { timer } from "rxjs";
+import { Observable } from "rxjs/internal/Observable";
+import { environment } from "../../../environments/environment";
 
 @Component({
-  selector: 'app-display1',
-  templateUrl: './display1.component.html',
-  styleUrls: ['./display1.component.scss'],
+  selector: "app-display1",
+  templateUrl: "./display1.component.html",
+  styleUrls: ["./display1.component.scss"],
 })
 export class Display1Component implements OnInit {
   public results: InterfaceScreenCalledQueue[]; //
-  constructor(
-    public getDataServiceService: GetDataServiceService,
-    private http: HttpClient
-  ) {}
+  public resultsAudio: InterfaceScreenCalledQueue[]; //
+  public resultsAudioDisplay: InterfaceScreenCalledQueue[];
+  public audio: HTMLAudioElement = new Audio();
+  constructor(public getDataServiceService: GetDataServiceService, private http: HttpClient) {}
 
-  ngOnInit(): void {
-    const source = timer(100, 1000);
-    // console.log(source);.
-    const subscribe = source.subscribe((val) => {
-      this.loadData(val);
-    });
-
-    this.announceQueuing(source);
+  ngOnInit() {
+    this.screenCalledQueuAudio();
+    // this.announceQueuing(source);
 
     // this.results.su
     // console.log(subscribe);
+    // const source = timer(100, 2000);
+    // // console.log(source);.
+    // const subscribe = await source.subscribe(async (val) => {
+    //   console.log('loaddata_' + new Date());
+    this.loadData(1);
+    // });
   }
 
-  loadData(val: any) {
+  async loadData(val: any) {
     // this.results = data1;
+    // console.log('loaddata2_' + new Date());
+    await this.timeout(2000);
     this.getDataServiceService
-      .getDisPay(
-        `screencalledqueu?uiDisplay=PharmacyScreenCalledQueue&idServicePoint=1`
-      )
+      .getDisPay(`screencalledqueue?uiDisplay=PharmacyScreenCalledQueue&idServicePoint=1`)
       .subscribe((data: any) => {
         this.results = data;
-        console.log(val);
-        console.log('*********----------*********');
-        console.log(data);
+        // console.log('loaddata3_' + new Date());
+        // console.log(val);
+        // // console.log('*********----------*********');
+        // console.log(data);
+        // // await this.timeout(1000);
+        if (this.results.length > 12) {
+          this.results.splice(12);
+        }
+        this.loadData(1);
         // console.log(this.announceQueuing());
       });
 
@@ -49,14 +57,72 @@ export class Display1Component implements OnInit {
     return val;
   }
 
-  announceQueuing(val: Observable<number>) {
-    val.subscribe((val) => {
-      console.log('val');
+  // announceQueuing(val: Observable<number>) {
+  //   val.subscribe((val) => {
+  //     console.log('val');
+  //   });
+  //   // console.log(val);
+  //   // setTimeout(function () {
+  //   //   console.log('setTimeout');
+  //   // }, 10000);
+  //   return 'setTimeout2';
+  // }
+  async screenCalledQueuAudio() {
+    this.getScreenCalledQueuDisplayAudio();
+    let resultsAudio: InterfaceScreenCalledQueue[] = this.resultsAudio;
+    if (resultsAudio && resultsAudio.length > 0) {
+      let nameQueue = resultsAudio[0].nameQueue;
+      let nameSpeakServiceChannel = resultsAudio[0].nameSpeakServiceChannel;
+      let idQueueGroup = resultsAudio[0].idQueueGroup;
+      let orderQueue = resultsAudio[0].orderQueue;
+      this.resultsAudioDisplay = resultsAudio;
+      this.resultsAudio = [];
+      console.log(
+        "🚀 ~ file: display1.component.ts ~ line 78 ~ Display1Component ~ screenCalledQueuAudio ~ nameServiceChannel",
+        nameSpeakServiceChannel
+      );
+      await this.getScreenCalledQueuAudio(
+        "ขอเชิญคิว " + idQueueGroup + " ' ' " + orderQueue + " ที่ช่องบริการ" + nameSpeakServiceChannel
+      );
+
+      // await this.timeout(10000);
+
+      //   await this.timeout(3000);
+      console.log("done!");
+      // await this.timeout(1000);
+      this.screenCalledQueuAudio();
+    } else {
+      await this.timeout(1000);
+      this.screenCalledQueuAudio();
+    }
+  }
+  getScreenCalledQueuDisplayAudio() {
+    this.getDataServiceService
+      .getDisPay(`screencalledqueueAudio?uiDisplay=PharmacyScreenCalledQueue&idServicePoint=1`)
+      .subscribe((data: any) => {
+        this.resultsAudio = data;
+        console.log("🚀 ~ file: display1.component.ts ~ line 90 ~ Display1Component ~ .subscribe ~ data", data);
+        // console.log(this.announceQueuing());
+      });
+  }
+  async getScreenCalledQueuAudio(text: string) {
+    try {
+      this.audio.src = "http://" + environment.KPHGTTSServerHost + `?text=` + text + "&lang=th";
+      await this.audio.load();
+      await this.audio.play();
+      return new Promise((resolve, reject) => {
+        this.audio.onerror = reject;
+        this.audio.onended = resolve;
+      });
+    } catch (error) {
+      return new Promise((resolve) => {
+        setTimeout(resolve, 2000);
+      });
+    }
+  }
+  timeout(ms: number) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
     });
-    // console.log(val);
-    // setTimeout(function () {
-    //   console.log('setTimeout');
-    // }, 10000);
-    return 'setTimeout2';
   }
 }
