9f66abf06c6a16b2211aafaa0262ee14b32a1ba4
apichat thongngoen <37826468+apichatthongngoen@users.noreply.github.com>
2021-03-20 15:20:44 +0700

    จัดรูปแบบ

--- src/app/display/display-routing.module.ts
+++ src/app/display/display-routing.module.ts
@@ -1,16 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-import { Display1Component } from './display1/display1.component';
-import { Display2Component } from './display2/display2.component';
-import { Display3Component } from './display3/display3.component';
-import { HomeComponent } from './home/home.component';
+import { Display1Component } from "./display1/display1.component";
+import { Display2Component } from "./display2/display2.component";
+import { Display3Component } from "./display3/display3.component";
+import { HomeComponent } from "./home/home.component";
 
 const routes: Routes = [
-  { path: 'display', component: Display1Component },
-  { path: 'display2', component: Display2Component },
-  { path: 'display3', component: Display3Component },
-  { path: 'home', component: HomeComponent },
+  { path: "display", component: Display1Component },
+  { path: "pharmacydisplay", redirectTo: "display", pathMatch: "full" },
+  { path: "display2", component: Display2Component },
+  { path: "display3", component: Display3Component },
+  { path: "home", component: HomeComponent },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
