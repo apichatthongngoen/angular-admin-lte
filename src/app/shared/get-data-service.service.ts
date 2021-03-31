a2002ba08fe96e01eb8528db57157858a2eccb2e
apichat thongngoen <37826468+apichatthongngoen@users.noreply.github.com>
2021-03-31 21:53:13 +0700

    ..

--- src/app/shared/get-data-service.service.ts
+++ src/app/shared/get-data-service.service.ts
@@ -1,12 +1,12 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, throwError, timer } from 'rxjs';
-import { retry, catchError } from 'rxjs/operators';
-import { QueueItem } from './interface/dataapi';
-import { environment } from '../../environments/environment';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable, throwError, timer } from "rxjs";
+import { retry, catchError } from "rxjs/operators";
+import { QueueItem } from "./interface/dataapi";
+import { environment } from "../../environments/environment";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class GetDataServiceService {
   baseurl: string = `http://` + environment.apiHost + `/api`;
@@ -16,48 +16,42 @@ export class GetDataServiceService {
   // Http Headers
   httpOptions = {
     headers: new HttpHeaders({
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     }),
   };
   // GET
   getDisPay(id: string): Observable<QueueItem> {
-    return this.http
-      .get<QueueItem>(this.baseurl + '/display/' + id)
-      .pipe(retry(10), catchError(this.errorHandl));
+    return this.http.get<QueueItem>(this.baseurl + "/display/" + id).pipe(retry(0), catchError(this.errorHandl));
   }
   GetIssue1(id: string) {
-    return this.http
-      .get(this.baseurl + '/' + id, { observe: 'response' })
-      .pipe(retry(1), catchError(this.errorHandl));
+    return this.http.get(this.baseurl + "/" + id, { observe: "response" }).pipe(retry(0), catchError(this.errorHandl));
   }
 
   postDisPay(url: string, data): Observable<QueueItem> {
     return this.http
-      .post<QueueItem>(
-        this.baseurl + '/display/' + url,
-        JSON.stringify(data),
-        this.httpOptions
-      )
-      .pipe(retry(10), catchError(this.errorHandl));
+      .post<QueueItem>(this.baseurl + "/display/" + url, JSON.stringify(data), this.httpOptions)
+      .pipe(retry(0), catchError(this.errorHandl));
   }
 
   postAPI(url: string, data): Observable<QueueItem> {
     return this.http
-      .post<QueueItem>(
-        this.baseurl + '/' + url,
-        JSON.stringify(data),
-        this.httpOptions
-      )
-      .pipe(retry(10), catchError(this.errorHandl));
+      .post<QueueItem>(this.baseurl + "/" + url, JSON.stringify(data), this.httpOptions)
+      .pipe(retry(0), catchError(this.errorHandl));
+  }
+
+  postAPISetting(url: string, data): Observable<interfaceSetting> {
+    return this.http
+      .post<interfaceSetting>(this.baseurl + "/" + url, JSON.stringify(data), this.httpOptions)
+      .pipe(retry(0), catchError(this.errorHandl));
   }
   getKPHSMartCardReader(): Observable<QueueItem> {
     return this.http
-      .get<QueueItem>('http://' + environment.KPHSMartCardReaderHost)
+      .get<QueueItem>("http://" + environment.KPHSMartCardReaderHost)
       .pipe(retry(1), catchError(this.errorHandl));
   }
   // Error handling
   errorHandl(error) {
-    let errorMessage = '';
+    let errorMessage = "";
     if (error.error instanceof ErrorEvent) {
       // Get client-side error
       errorMessage = error.error.message;
