e4f4c376443433ad9aa42b280b7d03133db7dc2b
apichat thongngoen <37826468+apichatthongngoen@users.noreply.github.com>
2021-05-14 13:37:28 +0700

    การ routes pharmacydisplay

--- src/app/display/pharmacy-display/pharmacy-display-routing.module.ts
+++ src/app/display/pharmacy-display/pharmacy-display-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { Display1Component } from "../display1/display1.component";
 
 import { DisplayMainComponent } from "./display-main/display-main.component";
+import { QueueAllComponent } from "./queue-all/queue-all.component";
 
-const routes: Routes = [{ path: "pharmacdyisplay", component: DisplayMainComponent }];
+const routes: Routes = [
+  {
+    path: "pharmacydisplay",
+    children: [
+      { path: "", component: DisplayMainComponent },
+      { path: "pagequeueall", component: QueueAllComponent },
+      { path: "pagequeue", component: Display1Component },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
